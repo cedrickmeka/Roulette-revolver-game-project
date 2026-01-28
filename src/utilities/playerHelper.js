6c7a15a1e4509f97dc40db876299a925b516f9ac
cedrickmeka <cedrickmeka88@gmail.com>
2026-01-28 19:13:34 +0300

    updated tree

---
+++ src/utilities/playerHelper.js
@@ -0,0 +1,8 @@
+/*
+ * playerUtils.js
+ * ----------------
+ * Utility functions related to player identity in a two-player game.
+ *
+ * This file contains helper logic for determining relationships
+ * between players (e.g. identifying the opponent of the current player).
+ */
