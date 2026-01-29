ec204da1212b6a955d1846663ac89b85318e5025
cedrickmeka <cedrickmeka88@gmail.com>
2026-01-29 11:41:39 +0300

    shots per round

--- src/utilities/stageConfig.js
+++ src/utilities/stageConfig.js
@@ -1,6 +1,10 @@
-/**
- * stageConfig.js
- * ----------------
- * Configuration for stage escalation,
- * defining the number of trigger pulls per stage.
- */
+//Defining the number of trigger pulls per stage.
+
+const stageConfig = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4
+};
+
+export default stageConfig;
