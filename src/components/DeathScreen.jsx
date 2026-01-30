bb1181ab13e5e45d7d130812574330ae7483808d
Jim Ochieng <jimochieng64@gmail.com>
2026-01-30 07:16:44 +0300

    Add font-family for Death Screen and hover effects

--- src/components/DeathScreen.jsx
+++ src/components/DeathScreen.jsx
@@ -4,18 +4,25 @@
  * Displays when a player is eliminated
  * due to a gunshot or trivia failure.
  */
+import styles from "../styles/Styles.module.css";
 
 function DeathScreen() {
 	return (
-		<div className="border-2 w-150 m-auto text-center p-20 mt-40 rounded-4xl bg-black/80 shadow-xl shadow-gray-950/90">
+		<div className="border-2 w-150 m-auto text-center p-20 mt-40 rounded-4xl bg-black/80 shadow-xl shadow-gray-950/90 font-[Righteous]">
 			<p className="text-red-600 font-bold text-5xl">💀Game Over💀</p>
 			<small className="text-gray-300">You have failed</small>
 
 			<div className="flex justify-center align-middle gap-4 mt-7 mb-0">
-				<button className="border-2 bg-yellow-500 p-2 rounded-2xl w-20 text-white cursor-pointer" onClick={"#"}>
+				<button
+					className="border-2 bg-yellow-500 p-2 rounded-2xl w-20 text-white cursor-pointer"
+					onClick={"#"}
+				>
 					Retry
 				</button>
-				<button className="border-2 bg-gray-700 p-2 rounded-2xl w-20 text-white cursor-pointer" onClick={"#"}>
+				<button
+					className="border-2 bg-gray-700 p-2 rounded-2xl w-20 text-white cursor-pointer"
+					onClick={"#"}
+				>
 					Home
 				</button>
 			</div>
