2c9b7b37d59debc7611a351363d3cd9dfd04bdbd
Jim Ochieng <jimochieng64@gmail.com>
2026-02-01 14:30:01 +0300

    Resolved conflit in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-<<<<<<< HEAD
 createRoot(document.getElementById("root")).render(
-=======
-
-
-createRoot(document.getElementById('root')).render(
->>>>>>> origin/main
   <StrictMode>
     <App />
   </StrictMode>,
