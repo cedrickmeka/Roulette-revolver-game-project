4a980317c7544af37126c959520d5cd68a2f4195
Jim Ochieng <jimochieng64@gmail.com>
2026-02-03 00:51:57 +0300

    Uncommented lines on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css";
 import App from './App.jsx'
-//import GameProvider from "./context/GameContext.jsx";
+import GameProvider from "./context/GameContext.jsx";
 import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/*<GameProvider/>
+    <GameProvider>
       <App />
-    {/*</GameProvider>*/}
+    </GameProvider>
   </StrictMode>,
 );
